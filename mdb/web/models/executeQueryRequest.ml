5951f0148e6e28e55032016300ae3c6b0c9ba166
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-11-29 22:27:37 +0100

    require executeQueryRequest.queryDefinition

--- mdb/web/models/executeQueryRequest.ml
+++ mdb/web/models/executeQueryRequest.ml
@@ -2,6 +2,6 @@ open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
 type t =
   { description : string option [@yojson.option]
-  ; query_definition : QueryDefinition.t option [@yojson.option] [@key "queryDefinition"]
+  ; query_definition : QueryDefinition.t [@key "queryDefinition"]
   }
 [@@deriving yojson]
