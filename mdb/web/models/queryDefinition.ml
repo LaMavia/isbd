93e2b416a7c858f7587fea5b0537ddb1a45358ab
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-01 20:23:49 +0100

    return SelectQuery

--- mdb/web/models/queryDefinition.ml
+++ mdb/web/models/queryDefinition.ml
@@ -4,15 +4,12 @@ type t =
 
 module Internal = struct
   let select_query_of_yojson json = QD_SelectQuery (SelectQuery.t_of_yojson json)
-
-  let copy_query_of_yojson json =
-    Printf.eprintf "\njson=%s\n" (Yojson.Safe.to_string json);
-    flush_all ();
-    QD_CopyQuery (CopyQuery.t_of_yojson json)
-  ;;
+  let copy_query_of_yojson json = QD_CopyQuery (CopyQuery.t_of_yojson json)
 end
 
-let t_of_yojson json = Utils.Yj.alt Internal.[ copy_query_of_yojson ] json
+let t_of_yojson json =
+  Utils.Yj.alt Internal.[ select_query_of_yojson; copy_query_of_yojson ] json
+;;
 
 let yojson_of_t = function
   | QD_SelectQuery q -> SelectQuery.yojson_of_t q
--- mdb/web/routes/queries/queries.ml
+++ mdb/web/routes/queries/queries.ml
@@ -1,6 +1,6 @@
 let routes =
   let open Dream in
   [ get "/queries" Queries_get.handler
-  ; scope "/query" [] [ get "/:query_id" Query.Get.handler; post "" Query.Post.handler ]
+  ; scope "/query" [] Query.[ get "/:query_id" Get.handler; post "" Post.handler ]
   ]
 ;;
