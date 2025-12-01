4166f6071b2a0ef0ca4a49d8f5678105a8f65adf
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-01 14:08:46 +0100

    add GET /system/info

--- mdb/web/routes/system_.ml
+++ mdb/web/routes/system_.ml
@@ -1,5 +1,13 @@
 let routes =
   let open Dream in
-  let open Utils.Placeholder in
-  [ get "/system/info" unimplemented ]
+  [ get "/system/info" (fun _ ->
+      let open Models.SystemInformation in
+      { author = Some "Zuzanna Surowiec"
+      ; version = "0.0.1"
+      ; interface_version = Some "1.0.0"
+      }
+      |> [%yojson_of: t]
+      |> Yojson.Safe.to_string
+      |> json ~status:`OK)
+  ]
 ;;
