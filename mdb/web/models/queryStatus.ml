29883a33c8c70067df60983e75c6a3de62201353
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-01 14:45:12 +0100

    fix QueryStatus.t yojson

--- mdb/web/models/queryStatus.ml
+++ mdb/web/models/queryStatus.ml
@@ -1,7 +1,28 @@
 type t =
-  | Created [@name "CREATED"]
-  | Planning [@name "PLANNING"]
-  | Running [@name "RUNNING"]
-  | Completed [@name "COMPLETED"]
-  | Failed [@name "FAILED"]
-[@@deriving yojson]
+  | Created
+  | Planning
+  | Running
+  | Completed
+  | Failed
+
+let t_of_yojson (json : Yojson.Safe.t) : t =
+  match json with
+  | `String "CREATED" -> Created
+  | `String "PLANNING" -> Planning
+  | `String "RUNNING" -> Running
+  | `String "COMPLETED" -> Completed
+  | `String "FAILED" -> Failed
+  | v ->
+    Yojson.json_error
+    @@ Printf.sprintf
+         "Expected «CREATED | PLANNING | RUNNING | COMPLETED | FAILED» but got «%s»"
+         (Yojson.Safe.to_string v)
+;;
+
+let yojson_of_t = function
+  | Created -> `String "CREATED"
+  | Planning -> `String "PLANNING"
+  | Running -> `String "RUNNING"
+  | Completed -> `String "COMPLETED"
+  | Failed -> `String "FAILED"
+;;
