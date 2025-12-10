3e9f92f1a5cdd82fc26f926b2bcaf7373fd9fe54
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-11 00:30:07 +0100

    mark delete dirty

--- mdb/web/routes/tables/table/delete.ml
+++ mdb/web/routes/tables/table/delete.ml
@@ -1,6 +1,9 @@
+open Middleware
+
 let handler (req : Dream.request) =
-  let ms = Dream.field req Middleware.MetastoreMiddleware.field |> Option.get in
+  let ms = Dream.field req MetastoreMiddleware.field |> Option.get in
   let table_id = Dream.param req "table_id" |> Core.Uuid.of_string in
   Metastore.Store.drop_table table_id ms;
+  MetastoreMiddleware.mark_dirty req;
   Dream.empty `OK
 ;;
