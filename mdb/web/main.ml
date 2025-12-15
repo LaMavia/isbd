142df8a9e54e571fad85bb1b6b46390e1ff6abb3
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-15 17:27:58 +0100

    formatting

--- mdb/web/main.ml
+++ mdb/web/main.ml
@@ -7,19 +7,19 @@ let app =
   let metastore = Metastore.Store.load config in
   let worker_domain = Domain.spawn (Worker.Main.main metastore tq) in
   let uptime_clock = UptimeMiddleware.create () in
-  Sys.set_signal
-    Sys.sigint
-    Sys.(
-      Signal_handle
-        (fun _ ->
-          Dream.log "[EXIT] Stopping workers";
-          TaskQueue.stop tq;
-          Domain.join worker_domain;
-          Dream.log "[EXIT] Clearing results";
-          Metastore.Store.drop_all_results metastore;
-          Dream.log "[EXIT] Saving metastore";
-          Metastore.Store.save metastore;
-          exit 0));
+  Sys.(
+    set_signal
+      sigint
+      (Signal_handle
+         (fun _ ->
+           Dream.log "[EXIT] Stopping workers";
+           TaskQueue.stop tq;
+           Domain.join worker_domain;
+           Dream.log "[EXIT] Clearing results";
+           Metastore.Store.drop_all_results metastore;
+           Dream.log "[EXIT] Saving metastore";
+           Metastore.Store.save metastore;
+           exit 0)));
   AppConfigMiddleware.middleware config
   @@ UptimeMiddleware.middleware uptime_clock
   @@ MetastoreMiddleware.middleware metastore
