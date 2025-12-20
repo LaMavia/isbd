327ca9f13223d3a7e699295f4096984c18a5f6aa
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2025-12-20 16:20:24 +0100

    simplify mmap_result

--- mdb/utils/monad.ml
+++ mdb/utils/monad.ml
@@ -1,15 +1,10 @@
 let mmap_result f arr =
-  match List.map f arr with
-  | [] -> Ok []
-  | x :: xs ->
-    x
-    |> Result.map (fun x -> [ x ])
-    |> List.fold_right
-         (fun e u ->
-            match e with
-            | Ok r -> Result.map (List.cons r) u
-            | Error excs ->
-              Error (List.append excs (Result.fold ~ok:(Fun.const []) ~error:Fun.id u)))
-         (List.rev xs)
-    |> Result.map List.rev
+  List.fold_right
+    (fun e u ->
+       match e with
+       | Ok r -> Result.map (List.cons r) u
+       | Error excs ->
+         Error (List.append excs (Result.fold ~ok:(Fun.const []) ~error:Fun.id u)))
+    (List.map f arr)
+    (Ok [])
 ;;
