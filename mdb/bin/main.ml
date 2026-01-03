aa6cef212cee8713eefa2a1ff0b76bdd47784664
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2026-01-03 12:11:32 +0100

    fix env

--- mdb/bin/main.ml
+++ mdb/bin/main.ml
@@ -1,3 +1,3 @@
-let interface = Sys.getenv_opt "HOSTNAME" |> Option.value ~default:"0.0.0.0"
-let port = Sys.getenv_opt "PORT" |> Option.fold ~none:80 ~some:int_of_string
+let interface = Sys.getenv_opt "MDB_HOSTNAME" |> Option.value ~default:"0.0.0.0"
+let port = Sys.getenv_opt "MDB_PORT" |> Option.fold ~none:80 ~some:int_of_string
 let () = Dream.run ~interface ~port @@ Dream.logger @@ Web.Main.app ()
